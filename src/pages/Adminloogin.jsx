8dfccdb43f4a9d5a4f11ff554402613b6ffb341c
Afreenahmed14 <afreenahmed1412@gmail.com>
2026-06-13 07:20:34 +0530

    Add useEffect import to Adminloogin component

--- src/pages/Adminloogin.jsx
+++ src/pages/Adminloogin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 // import { login as apiLogin } from "../api/Api";
 
 import { signInWithEmailAndPassword } from "firebase/auth";
